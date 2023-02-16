5b9af952bc98ac1222a39a9464e49e735ba14cb9
chrisbrody <chrisbrody@codifyacademy.com>
2023-02-16 12:17:40 -0500

    displayed data on coaches page

--- src/pages/Coach.jsx
+++ src/pages/Coach.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import { DisplaySleeps } from '../components';
+import { DisplaySleeps, DisplayReadiness } from '../components';
 import { useStateContext } from '../context';
 
 import './coach.css'
@@ -8,6 +8,7 @@ import './coach.css'
 const Coach = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [sleeps, setSleeps] = useState([])
+  const [readiness, setReadiness] = useState([])
   const { address, contract, getSleepDays, getReadinessDays } = useStateContext();
 
   const fetchSleeps = async () => {
@@ -20,19 +21,33 @@ const Coach = () => {
     setIsLoading(false)
   }
 
+  const fetchData = async () => {
+    setIsLoading(true)
+
+    const data = await getReadinessDays()
+    setReadiness(data)
+    console.log(data);
+
+    setIsLoading(false)
+
+  }
+
   useEffect(() => {
     if(contract) fetchSleeps();
+    if(contract) fetchData();
   }, [address, contract])
   return (
     <>
       {!address && <div className='no_wallet'><p>no wallet connected</p></div>}
       {address && <div className="page__wrapper user"> Wallet Address: {address}</div>}
 
-      {!isLoading && sleeps.length === 0 && (
+      {!isLoading && sleeps.length === 0 && readiness.length === 0 && (
         <div>Loading client data</div>
       )}
 
       {!isLoading && address && sleeps && sleeps.map((sleep) => <DisplaySleeps {...sleep} />) }
+      
+      {!isLoading && address && readiness && readiness.map((readiness) => <DisplayReadiness {...readiness} />) }
       
     </>
     
